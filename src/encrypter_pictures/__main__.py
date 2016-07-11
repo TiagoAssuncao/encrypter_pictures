e0603526b28c43b40acb39a4780b50e82e21eb42
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-07-11 19:03:44 -0300

    Remove coments
    Signed-off-by: tiagoassuncao <thiagoribeironiquel@hotmail.com>

--- src/encrypter_pictures/__main__.py
+++ src/encrypter_pictures/__main__.py
@@ -1,28 +1,11 @@
-import argparse
-import encrypter_pictures
-
-
-# Create an argument parser.
-parser = argparse.ArgumentParser('encrypter-pictures')
-
+from gui import QApplication, ImageViewer
+import sys
 
 def main(args=None):
-    """Main entry point for your project.
-
-    Parameters
-    ----------
-
-    args : list
-        A of arguments as if they were input in the command line. Leave it None
-        use sys.argv.
-    """
-
-    args = parser.parse_args(args)
-
-    # Put your main script logic here
-    print('List of arguments:')
-    print(args)
-
+    app = QApplication(sys.argv)
+    imageViewer = ImageViewer()
+    imageViewer.showMaximized()
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    main(['-h'])
+    main()
