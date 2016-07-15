1c76b28feed021e5b094b5edd01a48992163ab8b
tiagoassuncao <thiagoribeironiquel@hotmail.com>
2016-07-14 21:45:39 -0300

    Adding a key with another image

--- src/encrypter_pictures/cript.py
+++ src/encrypter_pictures/cript.py
@@ -7,10 +7,14 @@ def cript(file_name):
     image = Image.open(file_name)
     width, height = image.size
 
+    key = Image.open("key.jpg")
+    key_width, key_height = key.size
+
     for x in range(width):
         for y in range(height):
             r, g, b = image.getpixel( (x,y) )
-            image.putpixel( (x,y), ((r - x)%255, (g + y)%255, (b + x)%255))
+            kr, kg, kb = key.getpixel( ((x)%key_width,(y)%key_height) )
+            image.putpixel( (x,y), ((r + kr)%256, (g + kg)%256, (b + kb)%256))
     image.save('out.jpg')
     output_path = os.path.abspath("out.jpg")
     return output_path
